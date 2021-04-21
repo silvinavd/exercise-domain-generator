e186eea378f1c7f10fcbb9fefdd37a86fdbc70ed
silvinavd <silvinavalverde8@gmail.com>
2021-04-21 13:25:32 +0000

    domain function

--- src/app.js
+++ src/app.js
@@ -6,6 +6,19 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+  let dom = [".com", ".us", ".net"];
+  let p = document.getElementById("domain");
+
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let j = 0; j < adj.length; j++) {
+      for (let k = 0; k < noun.length; k++) {
+        for (let l = 0; l < dom.length; l++) {
+          p.innerHTML += pronoun[i] + adj[j] + noun[k] + dom[l] + "<br>";
+        }
+      }
+    }
+  }
 };
